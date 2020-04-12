fd3e52492ec5f436e573354d62d685f2e941aa58
Ashutosh Dey <adey13@asu.edu>
2020-04-11 19:47:52 -0700

    updated prolog files. separated the one with and without parse tree

--- src/resources/Intepreter.pl
+++ src/resources/Intepreter.pl
@@ -1,96 +1,11 @@
 :- set_prolog_flag(double_quotes, string).
 
-digit --> [X], {number(X)} .
-alphaNumeric --> [X],{atom(X), X \= true, X \= false}.
-identifier --> [X],{string(X)}.
-identifier --> alphaNumeric .
 endLine --> [;].
 endPeriod --> [.].
 equal --> [=].
 var --> [var].
 begin --> [begin].
 end --> [end].
-
-program --> block,endPeriod.
-block --> begin, declrList,commandList,end.
-/*
-* Declaration Parsing
-*/
-declrList --> declR, endLine,declrList.
-declrList --> declR, endLine.
-declR --> var, identifier,equal,digit.
-declR --> var, identifier,equal,alphaNumeric.
-declR --> var, identifier,equal,booleanI.
-declR --> var, identifierList.
-identifierList --> identifier.
-identifierList --> identifier,[','], identifierList.
-
-/*
-* Commands Parsing
-*/
-commandList --> commandI,endLine,commandList.
-commandList --> commandI,endLine.
-commandI --> display.
-commandI --> commandInitialize.
-commandI --> ifEval.
-commandI --> forEval.
-commandI --> whileEval.
-commandI --> identifier ,[:,=], ternaryEval.
-
-commandInitialize --> identifier,[:,=],expr.
-commandInitialize --> identifier,[+,+].
-commandInitialize --> identifier,[-,-].
-
-
-ifEval -->[if],['('],booleanComb,[')'],[then],commandList, [else], commandList, [endif].
-ifEval -->[if],['('],booleanComb,[')'],[then],commandList, [endif].
-ternaryEval --> booleanComb,[?],expr,[:],expr.
-forEval --> [for],['('],commandInitialize,endLine,booleanComb,endLine,commandInitialize,[')'],[do],commandList,[endfor].
-forEval --> [for],identifier,[in],[range],['('],digit, [to],digit,[')'].
-whileEval --> [while],['('],booleanComb,[')'],[do],commandList,[endwhile].
-
-/*
-* Boolean Parsing
-*/
-booleanComb --> booleanI.
-booleanComb --> boolean.
-booleanI --> [true].
-booleanI --> [false].
-boolean --> [!],booleanComb.
-boolean --> expr,equal,equal,expr.
-boolean --> expr,[!],equal,expr.
-boolean --> expr,conditional,expr.
-
-
-conditional --> [>].
-conditional --> [<].
-conditional --> [>,=].
-conditional --> [<,=].
-conditional --> [and].
-conditional --> [or].
-:-table expr/2, term/2 .
-/*
-* Expression Parsing
-*/
-expr --> expr, [-], term.
-expr --> expr, [+], term.
-expr --> term .
-
-term --> term, [*], factor.
-term --> term, [/], factor.
-term --> factor.
-
-factor --> ['('],expr,[')'].
-factor --> digit.
-factor --> identifier.
-factor --> booleanI.
-factor --> alphaNumeric.
-
-display --> [display],['('],expr,[')'].
-
-
-
-
 
 digit(X) --> [X], {number(X)} .
 identifier(X) --> [X],{atom(X), X \= true, X \= false}.
@@ -170,7 +85,7 @@ term(t_div(X,Y)) --> term(X), [/], factor(Y).
 term(X) --> factor(X).
 
 factor(t_bracket(X)) --> ['('],expr(X),[')'].
-factor(t_digit(X)) --> digit(X).
+factor(t_numeric(X)) --> digit(X).
 factor(t_identifier(X)) --> identifier(X).
 factor(t_boolean(X)) --> booleanI(X).
 factor(t_string(X)) --> anystring(X).
